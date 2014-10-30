710b48ae74a0da82ad960eb9a3668a8642fae4ac
Alex Devero <deveroalex@gmail.com>
2014-10-30 09:29:09 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,13 @@ window.onload = function() {
   if (!Modernizr.input.autofocus) {
     var inputs = document.getElementsByTagName('input');
     inputs[0].focus();
-  }
+  };
   if (Modernizr.input.placeholder) {
     var labels = document.getElementsByTagName('label');
     for (var i = 0, j = labels.length; i < j; i++) {
       labels[i].classList.add('js-hidden');
-    }
-  }
+    };
+  };
   // If 'required' attribute is not supported
   if (!Modernizr.input.required) {
     var form = document.getElementById('js-form');
@@ -20,13 +20,26 @@ window.onload = function() {
       form.onsubmit = function() {
         return false;
       };
-    }
+    };
     form.onchange = function() {
       if (name.value.length > 1 && mail.value.length > 1 && phone.value.length > 1) {
         form.onsubmit = function() {
           return true;
         };
+      };
+    };
+    // fallback for input validation
+    mail.onblur = function() {
+      var mailTest = /^(([^<>()[]\.,;:s@"]+(.[^<>()[]\.,;:s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+      if (mail.value == '' || !mailTest.test(mail.value)) {
+        alert('Please enter valid email address.');
       }
-    }
-  }
+    };
+    phone.onblur = function() {
+      var phoneTest = /^\(?[0-9]{3}(\-|\)) ?[0-9]{3}-[0-9]{4}$/;
+      if (phone.value == '' || !phoneTest.test(phone.value)) {
+        alert('Please enter valid phone.');
+      }
+    };
+  };
 };
